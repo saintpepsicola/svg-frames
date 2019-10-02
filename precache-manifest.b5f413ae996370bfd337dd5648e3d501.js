c2bacab613a2ef0f682616642fef0c95e8b92c80
shiv <mehtashivam91@gmail.com>
2019-10-02 12:06:36 +0100

    Updates

---
+++ precache-manifest.b5f413ae996370bfd337dd5648e3d501.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca06d1cb9e29ad50e659c461bfe4d28f",
+    "url": "/svg-frames/index.html"
+  },
+  {
+    "revision": "1b3467986be765e47582",
+    "url": "/svg-frames/static/css/main.4885ec3d.chunk.css"
+  },
+  {
+    "revision": "8f584627863260d78dc7",
+    "url": "/svg-frames/static/js/2.8292363e.chunk.js"
+  },
+  {
+    "revision": "1b3467986be765e47582",
+    "url": "/svg-frames/static/js/main.23fabdcf.chunk.js"
+  },
+  {
+    "revision": "be597fe129fad429546b",
+    "url": "/svg-frames/static/js/runtime-main.518ff272.js"
+  }
+]);
